83b4ea7f274768c68a3a903e101e1d58adbac208
samoKrajci <samo.krajci@gmail.com>
2021-09-25 18:42:34 +0200

    add argparse for graph_editor

--- graph_editor/__main__.py
+++ graph_editor/__main__.py
@@ -1,9 +1,25 @@
 from screen import Screen
-import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Graph editor')
+
+parser.add_argument(
+    '-i', 
+    '--input-file', 
+    help='File with the input graph. Defauls to empty graph'
+)
+parser.add_argument(
+    '-o', 
+    '--output-file', 
+    help='File to save the output graph to. Defaults to \'graph.txt\''
+)
+
+class Args:
+    def __init__(self, input_file=None, output_file=None):
+        self.input_file = input_file
+        self.output_file = output_file
 
 if __name__ == '__main__':
+    args = parser.parse_args(namespace=Args)
     s = None
-    if len(sys.argv) > 1:
-        s = Screen(output_file=sys.argv[1])
-    else:
-        s = Screen()
+    s = Screen(input_file=args.input_file, output_file=args.output_file)
